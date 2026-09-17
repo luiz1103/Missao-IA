64c10ec7c9dca80d045b5e6b3df06fbee90a355e
luiz1103 <luiz.lipe.lima11@escola.pr.gov.br>
2026-09-17 10:18:39 -0300

    fh

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
 const perguntas = [
     {
         enunciado: "O cyberbullying é a violência virtual que ocorre geralmente com as pessoas tímidas e indefesas, ou simplesmente por não caírem na simpatia das mesmas. Como podemos evitá-lo",
