9cba12766e4e7402cc30464a445aa9a91a490280
luiz1103 <luiz.lipe.lima11@escola.pr.gov.br>
2026-09-24 10:30:46 -0300

    bf

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+import {aleatorio} from ‘./aleatorio.js’;
+import {perguntas} from ‘./perguntas.js;
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
@@ -31,7 +34,7 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++
     mostraPergunta();
